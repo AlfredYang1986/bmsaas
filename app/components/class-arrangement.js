6ea4a1a2d0d6a2858ef638944f1ca43ab0641329
AlfredYang1986 <yangyuanpig@gmail.com>
2018-10-26 12:26:49 +0800

    导出event 换个名字

--- app/components/class-arrangement.js
+++ app/components/class-arrangement.js
@@ -140,7 +140,7 @@ export default Component.extend({
             this.set('start_date', tmp);
         },
         appendBtnClicked() {
-            this.addClassBtnClicked();
+            this.onAddClassBtnClicked();
         }
     },
 
