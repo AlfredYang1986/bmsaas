1a793e1ae3a312fb46f2aac1be4d077279df29bf
AlfredYang1986 <yangyuanpig@gmail.com>
2018-10-27 17:18:22 +0800

    添加和修改 初步完成

--- app/controllers/edit/stud.js
+++ app/controllers/edit/stud.js
@@ -23,13 +23,67 @@ export default Controller.extend({
     urg_rs: '',
     urg_contact: '',
 
+    isPushing: false,
+
     actions: {
         saveInputBtnClicked () {
             console.log('save the date');
-            this.model.stud.me.set('name', this.chd_name);
+
+            if (!this.studValidate()) {
+                alert('something wrong !');
+                return;
+            }
+
+            debugger
+            let stud = null;
+            if (this.isPushing) {
+                stud = this.store.createRecord('bmstud', {
+                    id: this.guid()
+                })
+                let per_stud = this.store.createRecord('bmperson', {
+                    id: this.guid()
+                })
+                let per_gard = this.store.createRecord('bmperson', {
+                    id: this.guid()
+                })
+                let per_urg = this.store.createRecord('bmperson', {
+                    id: this.guid()
+                })
+                let gard = this.store.createRecord('bmgardian', {
+                    id: this.guid()
+                })
+                let urg = this.store.createRecord('bmurgent', {
+                    id: this.guid()
+                })
+                gard.set('me', per_gard);
+                urg.set('me', per_urg);
+                stud.set('me', per_stud);
+                stud.set('urgent', urg);
+            } else {
+                stud = this.model.stud;
+            }
+            // this.model.stud.me.set('name', this.chd_name);
+            stud.me.set('name', this.chd_name);
             // TODO: 其他的一些属性的修改都在这里解决
 
-            this.transitionToRoute('detail.stud', this.model.stud.id);
+            if (this.isPushing) {
+                this.transitionToRoute('stud');
+            } else {
+                this.transitionToRoute('detail.stud', stud.id);
+            }
+        },
+    },
+
+    studValidate() {
+        return this.chd_name.length != 0;
+    },
+
+    guid() {
+        function s4() {
+          return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
         }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
     }
 });
