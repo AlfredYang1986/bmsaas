e2e714f199eaa0bee7e5730a77525186c5782b65
wodelu <1058744843@qq.com>
2018-10-28 17:25:06 +0800

    在做体验详情

--- app/controllers/detail/experience.js
+++ app/controllers/detail/experience.js
@@ -1,13 +1,22 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+    sessionArrange: true,
+    activityDetail: false,
     actions: {
         addSession() {
             this.transitionToRoute('edit.session');
         },
         viewExperDetail() {
-            console.log(111)
             this.transitionToRoute('detail.experience-detail')
+        },
+        sessionArrange() {
+            this.set('sessionArrange', true);
+            this.set('activityDetail', false);
+        },
+        activityDetail() {
+            this.set('sessionArrange', false);
+            this.set('activityDetail', true);
         }
     }
 });
--- app/routes/detail/classes.js
+++ app/routes/detail/classes.js
@@ -1,6 +1,18 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+    // mock_data: service(),
+    // model() {
+    //     this.mock_data.sureTech();
+    //     let techs = this.store.peekAll('bmtech');
+    //
+    //     return RSVP.hash({
+    //             techs: techs
+    //         })
+    // },
     model() {
         return {
             teacherList:[{
@@ -70,7 +82,16 @@ export default Route.extend({
                 week:"周二",
                 time:"10：00~12：00",
                 teacherName:"a老师"
-            }]
+            }],
+            studentTableHead: [{
+                title:"姓名"
+            },{
+                title:"性别"
+            },{
+                title:"年龄"
+            },{
+                title:"联系方式"
+            },]
         }
     }
 });
