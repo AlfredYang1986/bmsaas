52b5e2c33f249c554292a59e5e748d8422268230
AlfredYang1986 <yangyuanpig@gmail.com>
2018-10-29 18:18:45 +0800

    check box @wordlu 写样式

--- app/components/checkbox-label.js
+++ app/components/checkbox-label.js
@@ -2,43 +2,20 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
-    positionalParams: ['title'],
-    checkedNumber: "2",
+    positionalParams: ['title', 'limit', 'candidate', 'titlecss', 'btncss'],
+    titlecss: '',
+    btncss: '',
     count: 0,
     actions: {
-        choicetest() {
-            let checkedArray = [];
-            let choicearr = document.getElementsByTagName('label');
-            for(var i=0;i<choicearr.length;i++) {
-                console.log(choicearr.length);
-                console.log(choicearr);
+        onOneUnselected(ele) {
+            this.set('count', this.count - 1);
+            ele.set('siSelected', false);
+        },
+        onOneSelected(ele) {
+            if (this.count < this.limit) {
+                this.set('count', this.count + 1);
+                ele.set('siSelected', true);
             }
-
-            var count = this.get('count');
-            if(count == 0) {
-                this.set('select','ok');
-                this.set('count',1);
-            } else if(count == 1){
-                this.set('select','no');
-                this.set('count',0);
-            }
-
-            var a=0;
-            var num = 2;
-        	for(var i=0;i<choicearr.length;i++)
-        		if(choicearr[i].select == 'ok'){
-                    console.log(choicearr[i].checked)
-        			a=a+1;
-        		}
-            	if(a==num){
-            		for(var i=0;i<choicearr.length;i++)
-            			if(!choicearr[i].checked)
-            				choicearr[i].disabled='disabled';
-            	}else{
-            		for(var i=0;i<choicearr.length;i++)
-            			choicearr[i].removeAttribute('disabled');
-            	}
         }
     }
-
 });
