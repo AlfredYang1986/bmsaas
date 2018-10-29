fc3e2313ee5ba9325182c1a33b092da49adfb892
Chenghao Sun <601330984@qq.com>
2018-10-29 11:18:02 +0800

    学员加上validation

--- app/controllers/edit/stud.js
+++ app/controllers/edit/stud.js
@@ -30,7 +30,7 @@ export default Controller.extend({
             console.log('save the date');
 
             if (!this.studValidate()) {
-                alert('something wrong !');
+                alert('必填项不能为空！');
                 return;
             }
 
@@ -63,6 +63,13 @@ export default Controller.extend({
             }
             // this.model.stud.me.set('name', this.chd_name);
             stud.me.set('name', this.chd_name);
+            stud.me.set('nickname', this.chd_nickname);
+            stud.me.set('gender', this.chd_gender);
+            stud.set('school', this.chd_school);
+            stud.guardian.me.set('name', this.par_name);
+            stud.guardian.me.set('nickname', this.par_nickname);
+            stud.guardian.set('rs', this.par_rs);
+            stud.guardian.me.set('contact', this.par_contact);
             // TODO: 其他的一些属性的修改都在这里解决
 
             if (this.isPushing) {
@@ -75,7 +82,20 @@ export default Controller.extend({
     },
 
     studValidate() {
-        return this.chd_name.length != 0;
+        let valiFlag = true;
+        if (this.chd_name.length == 0 ||
+          this.chd_nickname.length == 0 ||
+          this.chd_gender.length == 0 ||
+          this.chd_gender_str.length == 0 ||
+          this.chd_school.length == 0 ||
+          this.par_name.length == 0 ||
+          this.par_nickname.length == 0 ||
+          this.par_rs.length == 0 ||
+          this.par_contact.length == 0) {
+            valiFlag = false;
+        }
+        return valiFlag;
+        // return this.chd_name.length != 0;
     },
 
     guid() {
