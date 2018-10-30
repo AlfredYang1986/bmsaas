3e6120ccb4e2e64f5ee4205b3e4d738b7bc066af
Faiz <891366402@qq.com>
2018-10-30 22:19:13 +0800

    radio box

---
+++ app/components/bm-radio-box.js
@@ -0,0 +1,13 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+    tagName: '',
+    positionalParams: ['name', 'text', 'inputVal', 'isChecked', 'disabled'],
+    actions: {
+        choose() {
+            let attrs = this.get('attrs');
+            attrs.isChecked = this.isChecked
+            this.sendAction('radioClick', attrs)
+        }
+    }
+});
