6d84a0286ec0d1f2c85dcf95accabda03b9698fd
Faiz <891366402@qq.com>
2018-10-31 00:49:47 +0800

    体验

--- app/controllers/edit/experience.js
+++ app/controllers/edit/experience.js
@@ -33,6 +33,8 @@ export default Controller.extend({
             this.set('isCheckAgeInput', true);
             this.set('radioisChecked', false);
             this.set('age_range', "");
+            this.set('act_alb', 0);
+            this.set('act_aub', 0);
         },
         activitySort() {
             this.set('noSortChoose', false);
@@ -44,6 +46,27 @@ export default Controller.extend({
             this.set('experience', true);
             this.set('activity', false);
         },
+        setCat(cat) {
+            this.set('act_cat', cat);
+        },
+        gainsInputs(data) {
+            let darry = data.map(e => e.text)
+            this.set('gainsInputArray', darry);
+            // this.model.act.set('gains', darry)
+            // window.console.info(this.model.act.gains)
+        },
+        offeredInputs(data) {
+            let darry = data.map(e => e.text)
+            this.set('offeredInputArray', darry);
+            // this.model.act.set('offered', darry)
+            // window.console.info(this.model.act.offered)
+        },
+        neededInputs(data) {
+            let darry = data.map(e => e.text)
+            this.set('neededInputArray', darry);
+            // this.model.act.set('needed', darry)
+            // window.console.info(this.model.act.needed)
+        },
         sortNext() {
             this.set('selectNav', this.selectNav + 1);
         },
@@ -52,6 +75,9 @@ export default Controller.extend({
                 alert('something wrong!');
                 return;
             }
+            this.set('act_alb', parseInt(this.age_range.split('-')[0]));
+            this.set('act_aub', parseInt(this.age_range.split('-')[1]));
+
             this.set('selectNav', this.selectNav + 1);
         },
         contentNext() {
@@ -96,7 +122,6 @@ export default Controller.extend({
             //     alert('something wrong!');
             //     return;
             // }
-
             let act = null;
             if (this.isPushing) {
                 act = this.store.createRecord('bmactivityinfo', {
@@ -108,49 +133,32 @@ export default Controller.extend({
 
             // TODO: 其他的一些属性修改都在这里解决
             act.set('name', this.act_name);
-
-            act.set('gains', this.gainsInputArray);
-            act.set('offered', this.offeredInputArray);
-            act.set('needed', this.neededInputArray);
-            
-            // TODO: 其他的一些属性修改都在这里解决
+            act.set('cat', this.act_cat);
             act.set('length', this.act_length);
             act.set('planning', this.act_planning);
             act.set('description', this.act_des);
             act.set('ccontent', this.act_content);
-            // act.set('alb', this.act_alb);
-            // act.set('aub', this.act_aub);
+            act.set('alb', this.act_alb);
+            act.set('aub', this.act_aub);
             // act.set('gains', this.act_gains);
-            // act.set('cover', this.act_cover);
-            // act.set('imgs', this.act_imgs);
+            act.set('cover', this.act_cover);
+            act.set('imgs', this.act_imgs);
             // act.set('offered', this.act_offered);
             // act.set('needed', this.act_needed);
-            // act.set('notice', this.act_notice);
+            act.set('notice', this.act_notice);
+
+            act.set('gains', this.gainsInputArray);
+            act.set('offered', this.offeredInputArray);
+            act.set('needed', this.neededInputArray);
 
             if (this.isPushing) {
-                this.transitionToRoute('experience');
+                this.transitionToRoute('experienceOpen');
             } else {
                 this.transitionToRoute('detail.experience', act.id);
             }
+            this.set('modal4', true);
         },
-        gainsInputs(data) {
-            let darry = data.map(e => e.text)
-            this.set('gainsInputArray', darry);
-            // this.model.act.set('gains', darry)
-            // window.console.info(this.model.act.gains)
-        },
-        offeredInputs(data) {
-            let darry = data.map(e => e.text)
-            this.set('offeredInputArray', darry);
-            // this.model.act.set('offered', darry)
-            // window.console.info(this.model.act.offered)
-        },
-        neededInputs(data) {
-            let darry = data.map(e => e.text)
-            this.set('neededInputArray', darry);
-            // this.model.act.set('needed', darry)
-            // window.console.info(this.model.act.needed)
-        }
+        
     },
 
     isPushing: false,
@@ -163,11 +171,11 @@ export default Controller.extend({
     act_des: '',
     act_planning: '',
     act_content: '',
-    act_gains: [],
+    // act_gains: [],
     act_cover: '',
     act_imgs: [],
-    act_offered: [],
-    act_needed: [],
+    // act_offered: [],
+    // act_needed: [],
     act_notice: '',
 
     activityValidate() {
