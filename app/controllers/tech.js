28091531167395b093770626a0611bd6e68b3422
Chenghao Sun <601330984@qq.com>
2018-11-12 16:35:54 +0800

    老师添加修改  剩余问题：日期 性别

--- app/controllers/tech.js
+++ app/controllers/tech.js
@@ -7,7 +7,15 @@ export default Controller.extend({
             this.transitionToRoute('detail.tech', idx);
         },
         addTech() {
-            this.transitionToRoute('edit.tech')
+            this.transitionToRoute('edit.tech',"tech/push")
         }
-    }
+    },
+    guid() {
+        function s4() {
+          return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+      }
 });
