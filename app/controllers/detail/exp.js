d71de93a2ab75f91f8779a900408b6f145983f94
Chenghao Sun <601330984@qq.com>
2018-11-17 19:20:52 +0800

    修改表格内按钮样式完成

--- app/controllers/detail/exp.js
+++ app/controllers/detail/exp.js
@@ -4,23 +4,23 @@ import { inject as service } from '@ember/service';
 export default Controller.extend({
     bm_exp_service: service(),
     cur_idx: 0,
-    tableTitle: ["参与者","孩子","生日","性别", "联系方式", "渠道", "操作"],
+    tableTitle: ["参与者","孩子","生日","性别", "联系方式", "渠道", "", "操作"],
     tableContent: [
         {
             ind: "1",
             kid: "牛牛",
             birt: "1996-02-07",
             sex: "男",
-            num: "11111111111",
-            func: "小程序",
+            contact: "11111111111",
+            channels: "小程序",
         },
         {
             ind: '2',
             kid: "王二小",
             birt: "1996-03-25",
             sex: "男",
-            num: "11111111111",
-            func: "小程序",
+            contact: "11111111111",
+            channels: "小程序",
         }
     ]
 });
