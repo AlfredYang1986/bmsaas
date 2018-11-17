50e5c65596f3ace982c4e8dd01719db90e51dc5b
Chenghao Sun <601330984@qq.com>
2018-11-17 14:16:34 +0800

    更改图片弹窗按钮样式 场地详情加上图片拼图 图片数据取不到

--- app/controllers/detail/yard.js
+++ app/controllers/detail/yard.js
@@ -1,6 +1,30 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
     bm_yard_service: service(),
+    bmOss: service(),
+    isMorePic: false,
+    cover: computed("bm_yard_service" ,function(){
+        let client = this.bmOss.get('ossClient');
+        let yard = this.bm_yard_service.yard;
+        // console.log(yard);
+        if(yard !== null ) {
+            let url = client.signatureUrl(yard.cover);
+            // console.log(yard);
+            // console.log(yard.cover);
+            // console.log(url);
+            return url;
+        }
+        
+    }),
+    actions: {
+        openMorePic() {
+            this.set('isMorePic', true);
+        },
+        closeMorePic() {
+            this.set('isMorePic', false);
+        }
+    },
 });
