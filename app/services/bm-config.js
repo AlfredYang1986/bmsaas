3399c50dca5ae5ada4aaf29161c54662f913b3ac
AlfredYang1986 <yangyuanpig@gmail.com>
2018-11-19 02:17:02 +0800

    自动化发布的第一步

--- app/services/bm-config.js
+++ app/services/bm-config.js
@@ -2,9 +2,9 @@ import Service from '@ember/service';
 
 export default Service.extend({
     // host: 'altlys.com:8081',
-    // token: '8f874a970b4481894c28a4b6e4761289',
+    token: '8f874a970b4481894c28a4b6e4761289',
     host: '192.168.100.174:8080',
-    token: 'ce6af788112b26331e9789b0b2606cce',
+    // token: 'ce6af788112b26331e9789b0b2606cce',
 
     getUrl(ep) {
         return "http://" + this.host + ep;
