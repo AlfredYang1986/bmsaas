88342ed53d9eab580fdcc174b4263c9b4c3017bc
Chenghao Sun <601330984@qq.com>
2018-11-18 21:01:55 +0800

    课程详情图片拼图完成

--- app/components/bmpage/bm-session-info.js
+++ app/components/bmpage/bm-session-info.js
@@ -4,24 +4,4 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
     positionalParams: ['session'],
-    bmOss: service(),
-    isMorePic: false,
-    cover: computed("session" ,function(){
-        let client = this.bmOss.get('ossClient');
-        let session = this.session;
-        
-        if(session !== null ) {
-            let url = client.signatureUrl(this.session.cover);
-            return url;
-        }
-        
-    }),
-    actions: {
-        openMorePic() {
-            this.set('isMorePic', true);
-        },
-        closeMorePic() {
-            this.set('isMorePic', false);
-        }
-    },
 });
