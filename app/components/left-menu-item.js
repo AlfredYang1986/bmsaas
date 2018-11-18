73a4b3148612e57a2fdf81e8f1731cd700c6a5d4
Chenghao Sun <601330984@qq.com>
2018-11-18 14:50:34 +0800

    首页侧边栏导航栏调整了一版

--- app/components/left-menu-item.js
+++ app/components/left-menu-item.js
@@ -4,14 +4,38 @@ export default Component.extend({
     collapsed: false,
     manageClick: false,
     teachClick: false,
+    workClick: false,
+    expClick: false,
     expands: false,
     isInbox: false,
     isSpace: false,
+    isDeActive: false,
+    isActive: false,
     positionalParams: ['icon', 'title', 'expendable'],
     actions: {
         toggle2() {
             this.sendAction('toggle2');
-            if (this.get('title') == "管理") {
+            if(this.get('title') == "工作台") {
+                if(this.workClick) {
+                    this.set('workClick', false);
+                    this.set('expendable',true);
+                    this.set('expands',false);
+                } else {
+                    this.set('workClick', true);
+                    this.set('expendable',false);
+                    this.set('expands',true);
+                }
+            } else if(this.get('title') == "体验活动") {
+                if(this.expClick) {
+                    this.set('expClick', false);
+                    this.set('expendable',true);
+                    this.set('expands',false);
+                } else {
+                    this.set('expClick', true);
+                    this.set('expendable',false);
+                    this.set('expands',true);
+                }
+            } else if (this.get('title') == "管理中心") {
                 if(this.manageClick) {
                     this.set('manageClick', false);
                     this.set('expendable',true);
