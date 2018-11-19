e84a88183216945fcbf203df8510bd39299a20be
Chenghao Sun <601330984@qq.com>
2018-11-19 17:44:43 +0800

    图片弹窗样式微调 详情页组件抽出

---
+++ app/components/bmdetail/stud-basis-left-subtitle.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+    classNames: ['stud-basis-left-subtitle'],
+    positionalParams: ['gender' ,'dob'],
+    // tagName: '',
+});
