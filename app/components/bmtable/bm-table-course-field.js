2bc8815fc66b23ad1394b9eb94fe0317da663e93
Chenghao Sun <601330984@qq.com>
2018-11-19 20:22:21 +0800

    table外层抽出完成，接下来内层

---
+++ app/components/bmtable/bm-table-course-field.js
@@ -0,0 +1,11 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+    classNames: [],
+	positionalParams: ['title', 'attendees'],
+	actions: {
+		// setCurrentApply: function (params) {
+		// 	this.sendAction("action", params);
+		// },
+	}
+});
