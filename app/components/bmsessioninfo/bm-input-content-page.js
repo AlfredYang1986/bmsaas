0aceb85e77b1601b648370a9448a6f4123254784
wodelu <1058744843@qq.com>
2018-11-28 22:19:04 +0800

    完成了体验课时长的radio

--- app/components/bmsessioninfo/bm-input-content-page.js
+++ app/components/bmsessioninfo/bm-input-content-page.js
@@ -2,4 +2,25 @@ import Component from '@ember/component';
 
 export default Component.extend({
     positionalParams: ['session'],
+    inputChecked: 'checked',
+    inputNoChecked: '',
+    lengthIf: true,
+    actions: {
+        lengthInputSelected() {
+            if(this.inputChecked == '') {
+                this.set('lengthIf', true);
+                this.set('inputChecked', 'checked');
+                this.set('inputNoChecked', '');
+                this.set('session.length', 0)
+            }
+        },
+        lengthNoSelected() {
+            if(this.inputChecked == 'checked') {
+                this.set('lengthIf', false);
+                this.set('inputChecked', '');
+                this.set('inputNoChecked', 'checked');
+                this.set('session.length', -1)
+            }
+        },
+    }
 });
