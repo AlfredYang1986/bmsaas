fba993dbbe18b8ca82b2d9b9f7a8c2c9deb4b8e6
wodelu <1058744843@qq.com>
2018-11-30 11:48:48 +0800

    修改昨天详情页的遗留问题

--- app/components/bmpage/bm-session-info.js
+++ app/components/bmpage/bm-session-info.js
@@ -3,28 +3,5 @@ import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 
 export default Component.extend({
-    // positionalParams: ['session'],
-    // haveInc: true,
-    // haveAcq: true,
-    // haveCarry: true,
-    // haveNote: true,
-    // init() {
-    //     this._super(...arguments);
-    //     debugger
-    //     if(this.session != undefined) {
-    //         if(this.session.inc == '') {
-    //             this.set('haveInc', false);
-    //         };
-    //         if(this.session.acquisition == '') {
-    //             this.set('haveAcq', false)
-    //         }
-    //         if(this.session.carrying == '') {
-    //             this.set('haveCarry', false)
-    //         }
-    //         if(this.session.notice == '') {
-    //             this.set('haveNote', false)
-    //         }
-    //     }
-    //
-    // },
+    positionalParams: ['session'],
 });
