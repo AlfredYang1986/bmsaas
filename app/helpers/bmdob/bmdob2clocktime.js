6c29491b03f7ed27978ae205bde31cfa89fb06e0
Chenghao Sun <601330984@qq.com>
2018-12-10 10:58:03 +0800

    时间转换helper调整

--- app/helpers/bmdob/bmdob2clocktime.js
+++ app/helpers/bmdob/bmdob2clocktime.js
@@ -4,8 +4,7 @@ export function bmdobBmdob2clocktime(params/*, hash*/) {
   let span = params[0];
 	let dob = new Date(span);
 
-  // return dob.getHours() + ':' + dob.getMinutes() + ':' + dob.getSeconds();
-  return dob.getHours() + ':' + dob.getMinutes();
+  return (dob.getHours() < 10 ? '0' + dob.getHours() : dob.getHours()) + ':' + (dob.getMinutes() < 10 ? '0' + dob.getMinutes() : dob.getMinutes());
 }
 
 export default helper(bmdobBmdob2clocktime);
