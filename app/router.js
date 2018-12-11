5c3e0ae075b5bed9fee1547ce0a2d216f8f3d1d9
AlfredYang1986 <yangyuanpig@gmail.com>
2018-12-11 13:07:40 +0800

    修正一下牛逼的路径bug

--- app/router.js
+++ app/router.js
@@ -11,37 +11,37 @@ Router.map(function() {
 
   this.route('edit', function() {
     this.route('brand');
-    this.route('stud', { path: '/:studid' });
-    this.route('tech', { path: '/:techid' });
-    this.route('yard', { path: '/:yardid' });
+    this.route('stud', { path: '/stud/:studid' });
+    this.route('tech', { path: '/tech/:techid' });
+    this.route('yard', { path: '/yard/:yardid' });
     this.route('classes');
-    this.route('course', { path: '/:courseid' });
-    this.route('session', { path: '/:actid'});
+    this.route('course', { path: '/course/:courseid' });
+    this.route('session', { path: '/session/:actid'});
 
     this.route('actv', function() {
-      this.route('info', { path: '/:sessionid' });
+      this.route('info', { path: '/info/:sessionid' });
       this.route('rsb');
     });
 
     this.route('exp', function() {
-      this.route('info', { path: '/:sessionid' });
+      this.route('info', { path: '/info/:sessionid' });
       this.route('rsb');
     });
   });
 
   this.route('detail', function() {
-    this.route('stud', { path: '/:studid' });
-    this.route('tech', { path: '/:techid' });
-    this.route('yard', { path: '/:yardid' });
-    this.route('course', { path: '/:courseid' });
-    this.route('classes', { path: '/:clsid'} );
-    this.route('course-reserve', { path: '/:reid' });
-    this.route('actarrangement', { path: '/:actactid/:perid' });
-    this.route('exp', { path: '/:expid' });
-    this.route('exp-field', { path: '/:expfieldid/:reexpid' });
-    this.route('actv', { path: '/:actvid' });
-    this.route('actv-field', { path: '/:actvfieldid/:reactvid' });
-    this.route('expsession', { path: '/:eid/:sid' });
+    this.route('stud', { path: '/stud/:studid' });
+    this.route('tech', { path: '/tech/:techid' });
+    this.route('yard', { path: '/yard/:yardid' });
+    this.route('course', { path: '/course/:courseid' });
+    this.route('classes', { path: '/cls/:clsid'} );
+    this.route('course-reserve', { path: '/course-reserve/:reid' });
+    this.route('actarrangement', { path: '/actarrangement/:actactid/:perid' });
+    this.route('exp', { path: '/exp/:expid' });
+    this.route('exp-field', { path: '/exp-field/:expfieldid/:reexpid' });
+    this.route('actv', { path: '/actv/:actvid' });
+    this.route('actv-field', { path: '/actv-field/:actvfieldid/:reactvid' });
+    this.route('expsession', { path: '/expsession/:eid/:sid' });
   });
 
   this.route('stud');
