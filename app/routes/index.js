9243757c400682b643ee79cd4f713ed7e27f9669
AlfredYang1986 <yangyuanpig@gmail.com>
2018-12-11 13:24:40 +0800

    / redirect 到home页

---
+++ app/routes/index.js
@@ -0,0 +1,7 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+    beforeModel(/* transition */) {
+        this.transitionTo('home'); // Implicitly aborts the on-going transition.
+    }
+});
