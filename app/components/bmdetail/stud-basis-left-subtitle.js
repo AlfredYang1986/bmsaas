e9653d744149b941daf3a76a66f91691f1280ace
wodelu <1058744843@qq.com>
2018-12-12 17:34:52 +0800

    改完学生管理页面样式 开始拼数据

--- app/components/bmdetail/stud-basis-left-subtitle.js
+++ app/components/bmdetail/stud-basis-left-subtitle.js
@@ -3,5 +3,7 @@ import Component from '@ember/component';
 export default Component.extend({
     classNames: ['stud-basis-left-subtitle'],
     positionalParams: ['gender' ,'dob'],
+    male: 'https://bm-web.oss-cn-beijing.aliyuncs.com/icon_male%402x.png',
+    female: 'https://bm-web.oss-cn-beijing.aliyuncs.com/icon_female%402x.png',
     // tagName: '',
 });
--- app/routes/detail/stud.js
+++ app/routes/detail/stud.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
 // import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { A } from '@ember/array';
 
 export default Route.extend({
     // mock_data: service(),
@@ -11,8 +12,10 @@ export default Route.extend({
         this.bm_stud_service.set('studid', params.studid);
 
         return RSVP.hash({
-            studid: params.studid 
+            studid: params.studid,
+            tabs: A(['详细信息', 'tab']),
         })
+
     },
     setupController(controller, model) {
         this._super(controller, model);
