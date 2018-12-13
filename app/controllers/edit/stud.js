56c4b1e79b27a874f0e40597ea6b7af7b30a76f6
wodelu <1058744843@qq.com>
2018-12-13 14:53:13 +0800

    开始拼编辑

--- app/controllers/edit/stud.js
+++ app/controllers/edit/stud.js
@@ -3,13 +3,21 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+    init() {
+        this._super(...arguments);
+        this.bm_tech_service.queryLocalMultiObject();
+    },
+
     bm_stud_service: service(),
+    bm_tech_service: service(),
 
     genderCheck: ['男', '女'],
     isPushing: false,
+    origin:['学员转介绍', '电话推广', '小程序', '闲暇活动推广', '其他'],
 
     actions: {
         saveInputBtnClicked() {
+            debugger
             let that = this
             let callback = {
                 onSuccess: function(res) {
@@ -21,5 +29,22 @@ export default Controller.extend({
             }
             this.bm_stud_service.saveUpdate(callback);
         },
+        selectedTech() {
+            let sel = document.getElementById("techSelect");
+            this.set('sel', sel)
+            if (sel.selectedIndex == 0) {
+                this.set('bm_stud_service.stud.teacherName', null);
+            } else {
+                this.set('bm_stud_service.stud.teacherName', sel.options[sel.selectedIndex].value);
+            }
+        },
+        selectedOrigin() {
+            let sel = document.getElementById("originSelect");
+            if (sel.selectedIndex == 0) {
+                this.set('bm_stud_service.stud.sourceWay', null);
+            } else {
+                this.set('bm_stud_service.stud.sourceWay', sel.options[sel.selectedIndex].value);
+            }
+        }
     },
 });
