cf685241860e08b04f92b209fdeb21da2b732905
wodelu <1058744843@qq.com>
2018-12-17 10:55:00 +0800

    排课页面

--- app/controllers/arrange-class.js
+++ app/controllers/arrange-class.js
@@ -4,7 +4,7 @@ import { computed } from '@ember/object';
 
 export default Controller.extend({
     bm_yard_service: service(),
-    openFlag: false,
+    openFlag: true,
     selectedYard: '',
     refreshSelected: computed(function(){
         var sel = document.getElementById("yardselect");
