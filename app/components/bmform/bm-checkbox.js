97a73d7b3f3059dafe2f249bc581c8e5706fd503
Chenghao Sun <601330984@qq.com>
2018-12-22 14:32:55 +0800

    校区多选有问题

--- app/components/bmform/bm-checkbox.js
+++ app/components/bmform/bm-checkbox.js
@@ -1,19 +1,70 @@
 import Component from '@ember/component';
 import { A } from '@ember/array';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-    positionalParams: ['title', 'checkboxItem', 'checkboxTitle'],
+    positionalParams: ['title', 'checkboxItem', 'checkboxTitle', 'curItems'],
     checkboxTitle: '',
+    curItems: A([]),
     handledArr: A([]),
-
-    actions: {
-        onClick (value) {
-            if (this.handledArr.indexOf(value) == -1) {
-                this.handledArr.push(value)
-            } else {
-                this.handledArr.splice(this.handledArr.indexOf(value), 1)
+    checkStatus: '',
+    handledCheckboxItem: A([]),
+    // handledCheckboxItem: computed('curItems', function() {
+    //     let tempArr = [];
+    //     for(let idx = 0;idx < this.checkboxItem.length;idx++) {
+    //         let tmpObj = {};
+    //         tmpObj.value = this.checkboxItem[idx]
+    //         if (this.curItems != null) {
+    //             for(let innerIdx = 0;innerIdx < this.curItems.length;innerIdx++) {
+    //                 if (this.checkboxItem[idx] == this.curItems[innerIdx]) {
+    //                     tmpObj.status = 'checked';
+    //                 } else {
+    //                     tmpObj.status = '';
+    //                 }
+    //                 tempArr.push(tmpObj)
+    //                 }
+    //             } else {
+    //                 tmpObj.status = '';
+    //                 tempArr.push(tmpObj)
+    //             }
+    //         }
+    //     console.log(tempArr)
+    //     return tempArr
+    // }),
+    
+    didReceiveAttrs() {
+        let tempArr = [];
+        for(let idx = 0;idx < this.checkboxItem.length;idx++) {
+            let tmpObj = {};
+            tmpObj.value = this.checkboxItem[idx]
+            if (this.curItems != null) {
+                for(let innerIdx = 0;innerIdx < this.curItems.length;innerIdx++) {
+                    if (this.checkboxItem[idx] == this.curItems[innerIdx]) {
+                        tmpObj.status = 'checked';
+                    } else {
+                        tmpObj.status = '';
+                    }
+                    tempArr.push(tmpObj)
+                    }
+                } else {
+                    tmpObj.status = '';
+                    tempArr.push(tmpObj)
+                }
+                // this.handledCheckboxItem = tempArr;
             }
-            // console.log(this.handledArr)
-        }
+        console.log(tempArr)
+        this.set('handledCheckboxItem', tempArr)
     },
+    
+
+    // actions: {
+    //     onClick (value) {
+    //         if (this.handledArr.indexOf(value) == -1) {
+    //             this.handledArr.push(value)
+    //         } else {
+    //             this.handledArr.splice(this.handledArr.indexOf(value), 1)
+    //         }
+    //         // console.log(this.handledArr)
+    //     }
+    // },
 });
--- app/controllers/edit/yard.js
+++ app/controllers/edit/yard.js
@@ -15,7 +15,7 @@ export default Controller.extend({
 
     yardCandidate: A(['室内', '室外', '室内 + 室外']),
     surroundings: A(['写字楼', '社区', '购物中心', '学校', '其它']),
-    facilities: A(['写字楼', '社区', '购物中心', '学校', '其它']),
+    facilities: A(['实时监控', '防摔地板', '空气净化器', 'Wi-Fi', '门禁', '安全护栏', '新风系统', '停车场', '急救包', '安全插座', '加湿器', '电梯', '安全桌脚']),
     tagsCandi: A(['阅读区', '教学区', '家长休息区', '生活区', '寄存区', '户外活动区', '室内活动区']),
     handledArr: A([]),
 
@@ -40,6 +40,8 @@ export default Controller.extend({
                 this.handledArr.splice(this.handledArr.indexOf(value), 1)
             }
             // console.log(this.handledArr)
+            this.set('bm_yard_service.yard.facilities', this.handledArr)
+            console.log(this.bm_yard_service.yard.facilities)
         }
     },
 });
