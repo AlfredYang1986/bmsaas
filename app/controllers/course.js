45afa2d2520c28059eb343cee5a8f9a1f6f6ad4c
Chenghao Sun <601330984@qq.com>
2019-01-16 18:30:00 +0800

    关闭开发中模块准备发布

--- app/controllers/course.js
+++ app/controllers/course.js
@@ -3,7 +3,7 @@ import { inject as service } from '@ember/service';
 
 export default Controller.extend({
     bm_session_service: service(),
-    openFlag: true,
+    openFlag: false,
     actions: {
         cardClicked(idx) {
             this.transitionToRoute('detail.course', idx);
