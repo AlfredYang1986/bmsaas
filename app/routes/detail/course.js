f27d7b9a0cb8ac34b09137e8de33b02548f03d6c
AlfredYang1986 <yangyuanpig@gmail.com>
2019-01-18 17:55:17 +0800

    课程列表和详情

--- app/routes/detail/course.js
+++ app/routes/detail/course.js
@@ -1,18 +1,10 @@
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
-import { inject as service } from '@ember/service';
 
 export default Route.extend({
-    bm_session_service: service(),
-
     model(params) {
-        this.bm_session_service.set('sessionid', params.courseid);
         return RSVP.hash({
-                courseid: params.courseid
-            })
-    },
-    setupController(controller, model) {
-        this._super(controller, model);
-        this.bm_session_service.set('refresh_token', this.bm_session_service.guid());
+            course: this.store.findRecord('sessioninfo', params.courseid)
+        })
     },
 });
