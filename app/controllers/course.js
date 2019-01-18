ca47028a622cd46af0c98dc428ec59606795d0ba
AlfredYang1986 <yangyuanpig@gmail.com>
2019-01-18 17:48:54 +0800

    course list

--- app/controllers/course.js
+++ app/controllers/course.js
@@ -1,9 +1,6 @@
 import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
 
 export default Controller.extend({
-    bm_session_service: service(),
-    openFlag: true,
     actions: {
         cardClicked(idx) {
             this.transitionToRoute('detail.course', idx);
