77fcef9a2f5d4a75d504c687c3804b3304129a0b
AlfredYang1986 <yangyuanpig@gmail.com>
2019-01-18 15:37:18 +0800

    品牌详情

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,13 +3,12 @@ import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 
 export default Controller.extend({
-    bm_brand_service: service(),
     cur_idx: 0,
     bmOss: service(),
     logoPath: computed(function(){
         let client = this.bmOss.get('ossClient');
 
-        let url = client.signatureUrl(this.bm_brand_service.brand.logo);
+        let url = client.signatureUrl(this.model.brand.logo);
         return url;
     })
 });
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,18 +1,14 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 import RSVP from 'rsvp';
 import { A } from '@ember/array';
 
 export default Route.extend({
-    bm_brand_service: service(),
-    bm_login_service: service(),
+    // bm_brand_service: service(),
+    // bm_login_service: service(),
     model() {
         return RSVP.hash({
             tabs: A(['品牌信息']),
+            brand: this.store.findRecord('brand', "5c4178918fb8073d04e89793")
         })
-    },
-    setupController(controller, model) {
-        this._super(controller, model);
-        this.bm_brand_service.set('refresh_token', this.bm_brand_service.guid());
     },
 });
