ae5a6be0654fcfd9e86757b2005abe3c0a2f2ace
AlfredYang1986 <yangyuanpig@gmail.com>
2019-01-19 13:23:58 +0800

    添加

--- app/controllers/edit/actv/rsb.js
+++ app/controllers/edit/actv/rsb.js
@@ -3,23 +3,15 @@ import { inject as service } from '@ember/service';
 import { debug } from '@ember/debug';
 
 export default Controller.extend({
-    bm_actv_service: service(),
 
     isCreate: true,
     isPushing: false,
 
     actions: {
         saveCourseBtnClicked(/*idx*/) {
-            let that = this
-            let callback = {
-                onSuccess: function(res) {
-                    that.transitionToRoute('detail.actv', res.data.id);
-                },
-                onFail: function(/*err*/) {
-                    debug('error');
-                }
-            }
-            this.bm_actv_service.saveUpdate(callback);
+            this.model.si.save();
+            this.model.exp.set('sessioninfo', this.model.si);
+            this.model.exp.save();
         },
         reserveCourse() {
             this.transitionToRoute('courseReserve');
--- app/routes/edit/actv/rsb.js
+++ app/routes/edit/actv/rsb.js
@@ -1,15 +1,16 @@
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
 import { A } from '@ember/array';
-import { inject as service } from '@ember/service';
 
 export default Route.extend({
-    bm_actv_service: service(),
-
     model() {
-        this.bm_actv_service.set('actvid', 'actv/push');
+        let act = this.store.createRecord('reservableitem');
+        // this.set("status", )  // TODO: 体验课的status？1？2
+        let si = this.store.createRecord('sessioninfo');
+
         return RSVP.hash({
-                actvid: 'actv/push',
+                actv: act,
+                si: si,
                 describe: A(["活动分类","基本信息","内容介绍","与孩子互动", "活动图片",]),
                 guide: A(["宝贝将获得","将为宝贝提供","参与者需自带","陪同说明", "须知",]),
             })
@@ -18,7 +19,5 @@ export default Route.extend({
     setupController(controller, model) {
         this._super(controller, model);
         // controller.set('cur_page_idx', 0);
-        this.bm_actv_service.set('refresh_token', this.bm_actv_service.guid());
-        // controller.set('refresh_token', this.bm_exp_service.guid());
     },
 });
