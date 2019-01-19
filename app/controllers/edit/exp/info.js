3367e32110f25f932b47f284d0c380d039ef43fc
AlfredYang1986 <yangyuanpig@gmail.com>
2019-01-19 12:38:02 +0800

    edit.exp.info

--- app/controllers/edit/exp/info.js
+++ app/controllers/edit/exp/info.js
@@ -1,26 +1,24 @@
 import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
-import { debug } from '@ember/debug';
 
 export default Controller.extend({
 
-    bm_session_service: service(),
-    bm_exp_service: service(),
     isCreate: false,
     isPushing: false,
 
     actions: {
         saveCourseBtnClicked(/*idx*/) {
-            let that = this
-            let callback = {
-                onSuccess: function() {
-                    that.transitionToRoute('detail.exp', that.bm_exp_service.expid);
-                },
-                onFail: function(/*err*/) {
-                    debug('error');
-                }
-            }
-            this.bm_session_service.saveUpdate(callback);
+            // let that = this
+            // let callback = {
+            //     onSuccess: function() {
+            //         that.transitionToRoute('detail.exp', that.bm_exp_service.expid);
+            //     },
+            //     onFail: function(/*err*/) {
+            //         debug('error');
+            //     }
+            // }
+            // this.bm_session_service.saveUpdate(callback);
+            this.model.course.save();
+            this.transitionToRoute('exp');
         },
         reserveCourse() {
             this.transitionToRoute('courseReserve');
