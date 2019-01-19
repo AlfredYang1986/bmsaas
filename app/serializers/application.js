fe82e11496b037690d352fd763deb46dcde47f83
AlfredYang1986 <yangyuanpig@gmail.com>
2019-01-19 16:17:50 +0800

    想法

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -4,8 +4,6 @@ export default DS.JSONAPISerializer.extend({
     normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
         let normalizedDocument = this._super(...arguments);
     
-        // Customize document meta
-        // normalizedDocument.meta = camelCaseKeys(normalizedDocument.meta);
         let tr =  normalizedDocument.meta['query-res']
         let tp =  normalizedDocument.meta['total-page']
         localStorage.setItem(tr, tp);
