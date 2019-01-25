33c8f5a2923fab8e0dbc89632d984004be6542a6
wodelu <1058744843@qq.com>
2019-01-25 11:15:00 +0800

    修改版本

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -7,7 +7,7 @@ export default DS.JSONAPIAdapter.extend({
             // 'Authorization': 'bearer ' + document.cookie.split(";")[0].split("=")[1] //token验证，需要时揭开注释
         };
     }),
-    namespace: "v0", // 根据后端发布版本修改命名空间
+    namespace: "v2", // 根据后端发布版本修改命名空间
     urlForFindHasMany(id, modelName) {
         let baseUrl = this.buildURL(modelName, id);
         return `${baseUrl}/relationships`;
