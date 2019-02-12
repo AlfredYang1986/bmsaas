6c82a8e95ec6f1141d1a31f22c3a9ba1979b6ac9
Chenghao Sun <601330984@qq.com>
2019-02-12 16:46:38 +0800

    修复班级首页重新载入时数据不更新的BUG，接下来给排课加验证

--- app/routes/classes.js
+++ app/routes/classes.js
@@ -14,13 +14,33 @@ export default Route.extend({
         })
         return RSVP.hash({
             courses: this.store.query('sessioninfo', { "brand-id": localStorage.getItem("brandid"), "status": 2}),
-            // classes: this.store.query('class', { "brand-id": localStorage.getItem("brandid"), "status": 2, "flag": 0}),
+            classes: this.store.query('class', { "brand-id": localStorage.getItem("brandid"), "status": 2, "flag": 0}),
             yard: tmp,
         })
     },
 
-    setupController(controller, model) {
-        this._super(controller, model);
-        // this.controller.set('cls', model.classes);
+    // setupController(controller, model) {
+    //     this._super(controller, model);
+    //     this.controller.set('cls', model.classes);
+    //     window.console.log(this.controller.get('cls'));
+        
+    // }
+    activate() {
+        // window.console.log(this.controllerFor('classes'));
+        this.store.query('class', { "brand-id": localStorage.getItem("brandid"), "status": 2, "flag": 0}).then(res => {
+            this.controllerFor('classes').set("cls", res)
+        },
+        () => {
+            window.console.log("query failed")
+        })
+        // this.controllerFor('classes').set("cls", )
+        // window.console.log(this.get('controller'));
+        // window.console.log(this.get("controller").model);
+        // if (this.controller) {
+        //     this.controller.set('cls', this.model.classes);
+        //     window.console.log(this.model.classes);
+        //     window.console.log(this.controller.get('cls'));
+        //     debugger
+        // }
     }
 });
