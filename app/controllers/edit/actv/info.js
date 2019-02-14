0e1428741462808b7535887f23ef941737a15833
wodelu <1058744843@qq.com>
2019-02-14 14:44:23 +0800

    活动和体验课的年龄检查

--- app/controllers/edit/actv/info.js
+++ app/controllers/edit/actv/info.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
 
@@ -6,6 +7,13 @@ export default Controller.extend({
     isPushing: false,
 
     savePicDoneFlag: false,
+    toast: service(),
+    toastOptions: {
+        closeButton: false,
+        positionClass: 'toast-top-center',
+        progressBar: false,
+        timeOut: '2000',
+    },
 
     actions: {
         saveCourseBtnClicked(/*idx*/) {
@@ -18,27 +26,30 @@ export default Controller.extend({
                 that.set("savePicDoneFlag", false)
                 debug('error');
             }
-            if(this.model.course.images.length === 0) {
-                this.model.course.save().then(onSuccess, onFail);
-            } else {
-                this.model.course.images.forEach((item, index, arr) => {
-                    if(index + 1 == arr.length) {
-                        this.set("savePicDoneFlag", true);
-                    }
-                    if(item.dirtyType !== undefined) {
-                        item.save().then(() => {
+            if(that.model.course.alb <= that.model.course.aub) {
+                if(this.model.course.images.length === 0) {
+                    this.model.course.save().then(onSuccess, onFail);
+                } else {
+                    this.model.course.images.forEach((item, index, arr) => {
+                        if(index + 1 == arr.length) {
+                            this.set("savePicDoneFlag", true);
+                        }
+                        if(item.dirtyType !== undefined) {
+                            item.save().then(() => {
+                                if(this.savePicDoneFlag) {
+                                    this.model.course.save().then(onSuccess, onFail);
+                                }
+                            });
+                        } else {
                             if(this.savePicDoneFlag) {
                                 this.model.course.save().then(onSuccess, onFail);
                             }
-                        });
-                    } else {
-                        if(this.savePicDoneFlag) {
-                            this.model.course.save().then(onSuccess, onFail);
                         }
-                    }
-                });
-            }
-            // this.model.course.save().then(onSuccess, onFail);
+                    });
+                }
+            } else {
+                that.toast.error('', '请检查年龄信息', that.toastOptions);
+            }            
         },
         reserveCourse() {
             this.transitionToRoute('courseReserve');
--- app/controllers/edit/exp/rsb.js
+++ app/controllers/edit/exp/rsb.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
 
@@ -6,6 +7,13 @@ export default Controller.extend({
     isPushing: false,
     cur_cate_id: '',
     savePicDoneFlag: false,
+    toast: service(),
+    toastOptions: {
+        closeButton: false,
+        positionClass: 'toast-top-center',
+        progressBar: false,
+        timeOut: '2000',
+    },
 
     actions: {
         saveCourseBtnClicked(/*idx*/) {
@@ -15,7 +23,6 @@ export default Controller.extend({
             let onSuccess = function () {
                 let tmp = that.store.peekRecord('sessioninfo', that.model.si.id)
                 that.model.exp.set('sessioninfo', tmp);
-                that.model.exp.set()
                 that.model.exp.save()
                     .then((res) => {
                         that.transitionToRoute('detail.exp', res.id);
@@ -24,28 +31,32 @@ export default Controller.extend({
             }
             let onFail = function () {
             }
-            // this.model.si.save().then(onSuccess, onFail);
-            if(this.model.si.images.length === 0) {
-                this.model.si.save().then(onSuccess, onFail);
-            } else {
-                this.model.si.images.forEach((item, index, arr) => {
-                    if(item.dirtyType !== undefined) {
-                        item.save().then(() => {
-                            if(imgCount + 1 == arr.length) {
-                                this.set("savePicDoneFlag", true);
-                            }
-                            imgCount ++;
+            if(that.model.si.alb <= that.model.si.aub) {
+                if(this.model.si.images.length === 0) {
+                    this.model.si.save().then(onSuccess, onFail);
+                } else {
+                    this.model.si.images.forEach((item, index, arr) => {
+                        if(item.dirtyType !== undefined) {
+                            item.save().then(() => {
+                                if(imgCount + 1 == arr.length) {
+                                    this.set("savePicDoneFlag", true);
+                                }
+                                imgCount ++;
+                                if(this.savePicDoneFlag) {
+                                    this.model.si.save().then(onSuccess, onFail);
+                                }
+                            });
+                        } else {
                             if(this.savePicDoneFlag) {
                                 this.model.si.save().then(onSuccess, onFail);
                             }
-                        });
-                    } else {
-                        if(this.savePicDoneFlag) {
-                            this.model.si.save().then(onSuccess, onFail);
                         }
-                    }
-                });
+                    });
+                }
+            } else {
+                that.toast.error('', '请检查年龄信息', that.toastOptions);
             }
+
         },
         reserveCourse() {
             this.transitionToRoute('exp');
