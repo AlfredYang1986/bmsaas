63a45ed4e4b02f31a2b7510e7739e91664dbd886
wodelu <1058744843@qq.com>
2019-02-18 14:56:58 +0800

    课程详情页样式

--- app/routes/detail/course.js
+++ app/routes/detail/course.js
@@ -1,10 +1,23 @@
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
+import { A } from '@ember/array';
 
 export default Route.extend({
     model(params) {
         return RSVP.hash({
-            course: this.store.findRecord('sessioninfo', params.courseid)
+            course: this.store.findRecord('sessioninfo', params.courseid),
+            urls: A([
+                {
+                    "pageName":"课程",
+                    "link":"exp",
+                    "id":"",
+                },
+                {
+                    "pageName":"课程详情",
+                    "link":"",
+                    "id":"",
+                }
+            ]),
         })
     },
 });
