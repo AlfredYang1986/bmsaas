381eb7314fc422f6130263c8252dfb8475cf8295
Chenghao Sun <601330984@qq.com>
2019-02-20 14:47:56 +0800

    班级排课模块BUG修复

--- app/models/room.js
+++ app/models/room.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+    brandId: DS.attr('string', { defaultValue: localStorage.getItem("brandid")}),
     capacity: DS.attr('number', { defaultValue: 100}),
     roomType: DS.attr('number', { defaultValue: 0}),
     title: DS.attr('string', { defaultValue: ""}),
