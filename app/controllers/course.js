1e13a275b1cc67d10c8f207ee86426eacdf18b29
wodelu <1058744843@qq.com>
2019-02-22 15:41:45 +0800

    reservableitem和sessioninfo的分页查询

--- app/controllers/course.js
+++ app/controllers/course.js
@@ -1,13 +1,28 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
     openFlag: true,
+    course: computed(function() {
+        return this.store.query('sessioninfo', { 'page[number]': 1, 'page[size]': 16, 'status': 2, "brand-id": localStorage.getItem("brandid")});
+    }),
+    page_count: computed(function(){
+        return Number.parseInt(localStorage.getItem('reservableitems'));
+    }),
+    pagenum: 1,
     actions: {
         cardClicked(idx) {
             this.transitionToRoute('detail.course', idx);
         },
         addCourse() {
             this.transitionToRoute('edit.course');
+        },
+        handlePageChange(page_num) {
+            let that = this;
+            this.set('pagenum', page_num)
+            this.store.query('sessioninfo', { 'page[number]': page_num, 'page[size]': 16, 'status': 2,  "brand-id": localStorage.getItem("brandid")}).then((res) => {
+                that.set('course', res)
+            })
         }
     }
 });
--- app/routes/actv.js
+++ app/routes/actv.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model() {
-        return this.store.query('reservableitem', { 'status': 0, "brand-id": localStorage.getItem("brandid")});
+        // return this.store.query('reservableitem', { 'status': 0, "brand-id": localStorage.getItem("brandid")});
     }
 });
