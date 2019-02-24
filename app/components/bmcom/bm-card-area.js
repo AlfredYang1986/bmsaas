88b129de7044e6981efa4b065d755fcbc3e4efba
wodelu <1058744843@qq.com>
2019-02-24 13:54:19 +0800

    活动课程体验课一级分页完成

--- app/components/bmcom/bm-card-area.js
+++ app/components/bmcom/bm-card-area.js
@@ -3,16 +3,16 @@ import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
-    positionalParams: ['type'],
+    positionalParams: ['type', 'curPage'],
     store: service(),
-    course: computed(function() {
+    course: computed('curPage', function() {
         let result;
         if(this.type == 'exp') {
-            result = this.store.query('reservableitem', { 'page[number]': 1, 'page[size]': 16, 'status': 1, "brand-id": localStorage.getItem("brandid")})
+            result = this.store.query('reservableitem', { 'page[number]': this.curPage, 'page[size]': 16, 'status': 1, "brand-id": localStorage.getItem("brandid")})
         } else if(this.type == 'actv') {
-            result = this.store.query('reservableitem', { 'page[number]': 1, 'page[size]': 16, 'status': 0, "brand-id": localStorage.getItem("brandid")})
+            result = this.store.query('reservableitem', { 'page[number]': this.curPage, 'page[size]': 16, 'status': 0, "brand-id": localStorage.getItem("brandid")})
         } else if(this.type == 'course') {
-            result = this.store.query('sessioninfo', { 'page[number]': 1, 'page[size]': 16, 'status': 2, "brand-id": localStorage.getItem("brandid")});
+            result = this.store.query('sessioninfo', { 'page[number]': this.curPage, 'page[size]': 16, 'status': 2, "brand-id": localStorage.getItem("brandid")});
         }
 
         result.then((res) => {
--- app/controllers/course.js
+++ app/controllers/course.js
@@ -12,11 +12,7 @@ export default Controller.extend({
             this.transitionToRoute('edit.course');
         },
         handlePageChange(page_num) {
-            let that = this;
             this.set('pagenum', page_num)
-            this.store.query('sessioninfo', { 'page[number]': page_num, 'page[size]': 16, 'status': 2,  "brand-id": localStorage.getItem("brandid")}).then((res) => {
-                that.set('course', res)
-            })
         },
         refreshDataComplete(page_Count) {
             this.set('page_count', page_Count)
