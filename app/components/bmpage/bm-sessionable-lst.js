100a34034274ea1502d6d100790e10ef302781fc
Chenghao Sun <601330984@qq.com>
2019-02-26 12:17:13 +0800

    部分问题修复

--- app/components/bmpage/bm-sessionable-lst.js
+++ app/components/bmpage/bm-sessionable-lst.js
@@ -6,15 +6,29 @@ export default Component.extend({
     classNames: ['bm-sessionable-table'],
     positionalParams: ['titles', 'listData', 'resid', 'totalCount'],
     store: service(),
+    hasDataFlag: false,
+    attributeBindings: ['style'],
+    style: computed('hasDataFlag', function(){
+        if (this.hasDataFlag) {
+            return "";
+        } else {
+            return 'height:' + "calc(100vh - 356px);" + 
+            'margin-bottom:' + "0;";
+        }
+    }),
     listData: null,
     classes: computed('curPage', 'resid', 'totalCount', function() {
         let ps
         ps = this.store.query('class', { 'page[number]': this.curPage, 'page[size]': 20, "reservable-id": this.resid});
-        ps.then(() => {
+        ps.then(res => {
             let count = localStorage.getItem('classes-count')
             let page = Number.parseInt(localStorage.getItem('classes'));
-			this.onRefreshDataComplete(count+' '+page)
-
+            this.onRefreshDataComplete(count+' '+page)
+            if(res.length == 0) {
+                this.set("hasDataFlag", false);
+            } else {
+                this.set("hasDataFlag", true);
+            }
         })
         return ps
     }),
