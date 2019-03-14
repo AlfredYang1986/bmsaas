84896c88fa4c7ca42caeb15b5d2e517bbb68473a
Frank <wsgoodman@163.com>
2019-03-14 19:31:09 +0800

    ！！将 bp-components 改为了本地 link。完成了 后面的两个页面：教学中心/管理中心。

--- app/controllers/actv.js
+++ app/controllers/actv.js
@@ -3,32 +3,32 @@ import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
-    bm_error_service: service(),
-    actv: computed(function() {
-        let result;
-        result = this.store.query('reservableitem', { 'page[number]': 1, 'page[size]': 16, 'status': 0, "brand-id": localStorage.getItem("brandid")})
-        result.then(() => {
-        }, error => {
-            this.bm_error_service.handleError(error)
-        })
-        return result;
-    }),
-    page_count: computed(function(){
-        return Number.parseInt(localStorage.getItem('reservableitems'));
-    }),
-    pagenum: 1,
-    actions: {
-        cardClicked(idx) {
-            this.transitionToRoute('detail.actv', idx);
-        },
-        addCourse() {
-            // this.transitionToRoute('edit.course');
-        },
-        handlePageChange(page_num) {
-            this.set('pagenum', page_num)
-        },
-        refreshDataComplete(page_Count) {
-            this.set('page_count', page_Count)
-        }
-    }
+	bm_error_service: service(),
+	actv: computed(function () {
+		let result;
+		result = this.store.query('reservableitem', { 'page[number]': 1, 'page[size]': 16, 'status': 0, "brand-id": localStorage.getItem("brandid") })
+		result.then(() => {
+		}, error => {
+			this.bm_error_service.handleError(error)
+		})
+		return result;
+	}),
+	page_count: computed(function () {
+		return Number.parseInt(localStorage.getItem('reservableitems'));
+	}),
+	pagenum: 1,
+	actions: {
+		cardClicked(idx) {
+			this.transitionToRoute('detail.actv', idx);
+		},
+		addCourse() {
+			// this.transitionToRoute('edit.course');
+		},
+		// handlePageChange(page_num) {
+		//     this.set('pagenum', page_num)
+		// },
+		refreshDataComplete(page_Count) {
+			this.set('page_count', page_Count)
+		}
+	}
 });
