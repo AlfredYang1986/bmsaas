29d5eea46d2c6e039038abfbad86b878bac489bf
Chenghao Sun <601330984@qq.com>
2019-03-27 19:20:29 +0800

    订单详情中途

--- app/models/attachable.js
+++ app/models/attachable.js
@@ -13,4 +13,6 @@ export default DS.Model.extend({
     preferentialPrice: DS.attr('number'),
     signedPrice: DS.attr('number'),
     archive: DS.attr('number'),
+
+    students: DS.hasMany('student'),
 });
--- app/routes/detail/order.js
+++ app/routes/detail/order.js
@@ -1,4 +1,29 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
+import { A } from '@ember/array';
 
 export default Route.extend({
+
+    model(params) {
+        return RSVP.hash({
+            trans: this.store.find('transaction', params.orderid),
+        })
+    },
+    
+    setupController(controller, model) {
+        this._super(controller, model);
+        let urls = A([
+            {
+                "pageName":"订单管理",
+                "link":"order",
+                "id":"",
+            },
+            {
+                "pageName":"订单详情",
+                "link":"",
+                "id":"",
+            }
+        ])
+        this.controller.set("urls", urls)
+    }
 });
