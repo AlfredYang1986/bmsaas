3968ecca7bc78aca34fd3d9f8642d0a7aed0de30
wodelu <1058744843@qq.com>
2019-03-29 14:54:50 +0800

    去掉error

--- app/helpers/bm-order-total-discount.js
+++ app/helpers/bm-order-total-discount.js
@@ -1,13 +1,12 @@
 import { helper } from '@ember/component/helper';
 
 export function bmOrderTotalDiscount(params/*, hash*/) {
-    console.log(params[0])
     let result = 0;
     params[0].forEach(elem => {
         window.console.log(elem);
         result += elem.get("preferentialPrice");
         window.console.log(result);
-        
+
     });
     return result;
 }
